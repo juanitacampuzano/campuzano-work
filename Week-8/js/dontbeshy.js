966ada8a4e00dad260ac48aedbb81f6993b05b2b
juanitacampuzano <31905831+juanitacampuzano@users.noreply.github.com>
2017-10-23 18:26:45 -0400

    changes

--- Week-8/js/dontbeshy.js
+++
@@ -1,9 +0,0 @@
-
-$(window).scroll(function() {
-    var bdyheight = $("html").offset().top;
-    if (bdyheight <= "-630") {
-        $("#2 .dontbeshy, #2 .dontbeshy h3").animate({
-            opacity : 100%
-        }, 700);
-    }
-}); 
