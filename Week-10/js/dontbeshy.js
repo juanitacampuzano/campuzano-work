94110959d6a888268fa06c83de94f875943e3800
juanitacampuzano <31905831+juanitacampuzano@users.noreply.github.com>
2017-11-03 11:52:05 -0400

    changes

--- Week-10/js/dontbeshy.js
+++ Week-10/js/dontbeshy.js
@@ -1,9 +1,50 @@
+console.log('im linked')
+
+var dbsheight;
+var txtheight;
+
+$(window).resize(function(){
+    dbsheight = $('.dontbeshy').offset().top  
+})
+
+$(document).ready(function(){
+    dbsheight = $('.dontbeshy').offset().top
+//    console.log(dbsheight)
+})
+
+
+$(document).ready(function(){
+    txtheight = $('.workTxt').offset().top
+    console.log(txtheight)
+})
+
+ 
+//    console.log("im linked")
 
 $(window).scroll(function() {
-    var bdyheight = $("html").offset().top;
-    if (bdyheight <= "-630") {
-        $("#2 .dontbeshy, #2 .dontbeshy h3").animate({
-            opacity : 100%
-        }, 700);
+    var bdyheight = $(window).scrollTop();
+//    console.log(bdyheight)
+//    if (bdyheight >= 617) {
+//       console.log('im here')
+//        $(".dontbeshy").animate({
+//            opacity : 1.0
+//        }, 700);
+//    } else if(bdyheight = 0){
+//        $(".dontbeshy").css('opacity', '0')  
+//    }
+    if (bdyheight >= (dbsheight/2+100)) {
+        $(".homeContactMeBtn").addClass('bigger');
+        $(".dontbeshy").addClass('visible');
+    } else{
+        $('.homeContactMeBtn').removeClass('bigger');
+        $(".dontbeshy").removeClass('visible');
+    }
+    
+    if (bdyheight >= txtheight/2) {
+        $('.workTxt').addClass('appear');
+    } else{
+        $('.workTxt').removeClass('appear');
     }
 }); 
+
+
